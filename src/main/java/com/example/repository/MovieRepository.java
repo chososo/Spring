1efca5887d300dd143d15653c90d8f5f53c4eccc
chososo <a50242953@gmail.com>
2023-09-26 17:30:10 +0900

    4

--- src/main/java/com/example/repository/MovieRepository.java
+++ src/main/java/com/example/repository/MovieRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 // T > 매핑할 객체  Td > T값의 타입 // => movie 객체와 키값의 타입인 long
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByProductionYear(Integer productionYear);
 
     @Query("SELECT distinct m FROM Movie m LEFT JOIN FETCH m.actors a LEFT JOIN FETCH m.director d")
     List<Movie> findAllJpqlFetch();
--- src/main/java/com/example/service/MovieService.java
+++ src/main/java/com/example/service/MovieService.java
@@ -43,7 +43,7 @@ public class MovieService {
     // 다건 조회
     @Transactional
     public List<MovieResponse> getMovies(Integer overYear) {
-        List<Movie> movies = movieRepository.findAll();
+        List<Movie> movies = movieRepository.findAllJpqlFetch();
 
         return movies.stream().map(MovieResponse::of).toList();
     }
